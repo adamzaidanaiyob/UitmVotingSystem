a7f88127344dd2228641a47e0e028ce7ac192c3c
Adam Zaidan <87437190+adamzaidanaiyob@users.noreply.github.com>
2023-08-13 15:56:25 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2022 <a href="https://terengganu.uitm.edu.my/kualaterengganu/">UiTM KUALA TERENGGANU</a></strong>
+    </div>
+  
+</footer>
